396884c40e077a3419cbf6eb1a18516c3eecdc93
Sergio G <96071836+SGC11PRO@users.noreply.github.com>
2023-12-06 13:23:15 +0100

    Update InputManager.cs
    - Added Look Around Function

--- Scripts/InputManager.cs
+++ Scripts/InputManager.cs
@@ -10,12 +10,16 @@ public class InputManager : MonoBehaviour
     private PlayerInput.OnFootActions onFoot; //Referencia a las acciones asignadas en ese mapa de acción
 
     private PlayerMotor playerMotor; //Referencia al script de PlayerMotor
+    private PlayerLook playerLook; //Referencia al script de PlayerLook
 
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+
+        //[!] Scripts Referenciados [!]
         playerMotor = GetComponent<PlayerMotor>();
+        playerLook = GetComponent<PlayerLook>();
 
         //Asignar acción "Jump"
         //Cada vez que llamamos a la acción jump (pulsando espacio) => Ejecuta la funcion jump del playermotor
@@ -26,6 +30,12 @@ public class InputManager : MonoBehaviour
     {
         //Envía al script de PlayerMotor los valores del input para que lo utilice en la función de "PlayerMove"
         playerMotor.PlayerMove(onFoot.Movement.ReadValue<Vector2>()); //llama a la función de "PlayerMove" asignandole los valores del action map ("onFoot")
+    }
+
+    private void LateUpdate()
+    {
+        //Envia al script de PlayerLook los valores del map action "Look"
+        playerLook.Look(onFoot.Look.ReadValue<Vector2>());
     }
 
     //Enable Inputs
