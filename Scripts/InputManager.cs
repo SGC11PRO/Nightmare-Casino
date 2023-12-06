19cbca26123bccd13e977c00a1942e9f10543707
Sergio G <96071836+SGC11PRO@users.noreply.github.com>
2023-12-06 12:15:39 +0100

    Update InputManager.cs

--- Scripts/InputManager.cs
+++ Scripts/InputManager.cs
@@ -9,15 +9,19 @@ public class InputManager : MonoBehaviour
     private PlayerInput playerInput; //Referencia a la clase de C# generada en el InputActions
     private PlayerInput.OnFootActions onFoot; //Referencia a las acciones asignadas en ese mapa de acción
 
+    private PlayerMotor playerMotor; //Referencia al script de PlayerMotor
+
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+        playerMotor = GetComponent<PlayerMotor>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-
+        //Envía al script de PlayerMotor los valores del input para que lo utilice en la función de "PlayerMove"
+        playerMotor.PlayerMove(onFoot.Movement.ReadValue<Vector2>()); //llama a la función de "PlayerMove" asignandole los valores del action map ("onFoot")
     }
 
     //Enable Inputs
